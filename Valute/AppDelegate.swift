8458dab9d89645b02467ca606c95a321f1c692be
Ognjen <pavlovic.ognjen51@gmail.com>
2022-09-23 14:16:07 +0200

    added background image on controller

--- Valute/AppDelegate.swift
+++ Valute/AppDelegate.swift
@@ -10,10 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "ConverterController")
+        
+        window?.rootViewController = vc
+        
+        window?.makeKeyAndVisible()
         return true
     }
 
